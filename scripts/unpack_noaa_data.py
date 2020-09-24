c9bbb06807dd329cf11cedb7ef3d8f7384b33f31
Benjamin Stürmer <bstuermer@eno.de>
2020-09-24 16:04:36 +0200

    Ensure that NOAA_AMY outputs directory exists to prevent an error if it's missing when unpack_noaa_data.py is executed

--- scripts/unpack_noaa_data.py
+++ scripts/unpack_noaa_data.py
@@ -3,10 +3,16 @@
 import glob
 import gzip
 import shutil
+import os
 
 zipfiles = '../inputs/NOAA_ISD_Lite_Raw/*/*.gz'
-
 filelist = glob.glob(zipfiles, recursive=True)
+
+# Create the output directory if it doesn't already exist
+output_dir = os.path.join('..', 'outputs', 'NOAA_AMY')
+if not os.path.exists(output_dir):
+    print("Creating outputs directory " + output_dir)
+    os.mkdir(output_dir)
 
 for gzfile in filelist:
     # Grab the file name
@@ -14,5 +20,5 @@ for gzfile in filelist:
     filename_string = gzfile_string[0:-3]
     # Unpack gz file and send to csv
     with gzip.open(gzfile, 'rb') as f_in:
-        with open(str('../outputs/NOAA_AMY/'+filename_string), 'wb') as f_out:
+        with open(os.path.join(output_dir, filename_string), 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
