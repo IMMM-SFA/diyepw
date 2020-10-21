758c2ee6f566f177e8fde28bd895f0e54c986b53
Benjamin Stürmer <bstuermer@eno.de>
2020-10-21 17:58:23 +0200

    Moved functions into the diyepw package

--- lib/diyepw/diyepw/__init__.py
+++ lib/diyepw/diyepw/__init__.py
@@ -1,1 +1,5 @@
 from .typical_meteorological_year import TypicalMeteorologicalYear
+from .functions import clean_noaa_df, \
+    convert_to_station_pressure, \
+    handle_missing_values, \
+    split_list_into_contiguous_segments
