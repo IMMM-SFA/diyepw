24b537ae18d5d4106bd0f42b79141f5fa05c91b8
Benjamin Stürmer <bstuermer@eno.de>
2020-11-07 15:47:08 +0100

    Removed some debugging code and added comments

--- lib/diyepw/diyepw/get_noaa_isd_lite_file.py
+++ lib/diyepw/diyepw/get_noaa_isd_lite_file.py
@@ -10,20 +10,20 @@ def get_noaa_isd_lite_file(wmo_index:int, year:int):
     :param year:
     :return:
     """
+
+    # Retrieve the NOAA ISD Lite catalog for the requested year
     catalog_url = f"https://www1.ncdc.noaa.gov/pub/data/noaa/isd-lite/{year}/"
     with _request.urlopen(catalog_url) as response:
         html = response.read().decode('utf-8')
+
+    # Find the filename in the catalog that matches the requested WMO index
     match = _re.search(f'href="({wmo_index}-\d*-{year}.gz)"', html)
     file_name = match.groups()[0]
 
+    # Download the ISD Lite file and decompress it, then save the decompressed file
     isd_lite_file_url = catalog_url + file_name
     epw_gz_file_handle, epw_gz_file_path = _tempfile.mkstemp()
     with _request.urlopen(isd_lite_file_url) as response, open(epw_gz_file_handle, 'wb') as epw_gz_file:
         epw_gz_file.write(response.read())
-
     with _gzip.open(epw_gz_file_path, 'rb') as epw_gz_file, open('test.epw', 'w') as epw_file:
         epw_file.write(epw_gz_file.read().decode('utf-8'))
-
-
-
-get_noaa_isd_lite_file(727928, 2020)
