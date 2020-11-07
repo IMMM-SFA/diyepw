c192a3c493dc10b50b4175ced9613bf40ae41427
Benjamin Stürmer <bstuermer@eno.de>
2020-11-07 13:49:47 +0100

    Split the functions file into three smaller files, and reduced the number of functions exposed to users

--- lib/diyepw/diyepw/__init__.py
+++ lib/diyepw/diyepw/__init__.py
@@ -1,8 +1,4 @@
 from .meteorology import Meteorology
-from .functions import clean_noaa_df, \
-    convert_sea_level_pressure_to_station_pressure, \
-    handle_missing_values, \
-    split_list_into_contiguous_segments, \
-    analyze_noaa_isd_lite_files, \
-    convert_noaa_isd_lite_to_amy_epw_files, \
-    get_wmo_station_location
+from .convert_noaa_isd_lite_to_amy_epw_files import convert_noaa_isd_lite_to_amy_epw_files
+from .analyze_noaa_isd_lite_files import analyze_noaa_isd_lite_files
+from .get_wmo_station_location import get_wmo_station_location
