cce8e38be23ceb7d4e20b34d8f4f1b7d56aab279
Benjamin Stürmer <bstuermer@eno.de>
2020-11-09 18:32:10 +0100

    Updated NOAA ISD Lite fetcher to be a bit cleaner code, and to buffer the catalog HTML so that it's not downloading the same content repeatedly on multiple calls

--- lib/diyepw/diyepw/get_noaa_isd_lite_file.py
+++ lib/diyepw/diyepw/get_noaa_isd_lite_file.py
@@ -1,8 +1,11 @@
 import urllib.request as _request
 import re as _re
-import gzip as _gzip
 import os as _os
 import tempfile as _tempfile
+
+# Buffer for the EPW catalog, which is a large HTML file that we don't want to have to download anew every time
+# a new ISD Lite file is requested
+_catalog_html = None
 
 def get_noaa_isd_lite_file(wmo_index:int, year:int, output_dir:str = None) -> str:
     """
@@ -17,24 +20,25 @@ def get_noaa_isd_lite_file(wmo_index:int, year:int, output_dir:str = None) -> st
     if output_dir is None:
         output_dir = _tempfile.mkdtemp()
 
-    # Retrieve the NOAA ISD Lite catalog for the requested year
     catalog_url = f"https://www1.ncdc.noaa.gov/pub/data/noaa/isd-lite/{year}/"
-    with _request.urlopen(catalog_url) as response:
-        html = response.read().decode('utf-8')
 
-    # Find the filename in the catalog that matches the requested WMO index
-    match = _re.search(f'href="({wmo_index}-\d*-{year}\.gz)"', html)
+    # Retrieve the NOAA ISD Lite catalog for the requested year, if it hasn't already been downloaded
+    global _catalog_html
+    if _catalog_html is not None:
+        with _request.urlopen(catalog_url) as response:
+            _catalog_html = response.read().decode('utf-8')
+
+    # Find the filename in the catalog that matches the requested WMO index and year
+    match = _re.search(f'href="({wmo_index}-\d*-{year}\.gz)"', _catalog_html)
     if match is None:
         raise Exception(f"No ISD Lite file for WMO index {wmo_index} and year {year} could be found at {catalog_url}")
-    file_name_gz = match.groups()[0]
+    file_name = match.groups()[0]
 
-    # Download the ISD Lite file and decompress it, then save the decompressed file
-    isd_lite_file_url = catalog_url + file_name_gz
-    epw_gz_file_handle, epw_gz_file_path = _tempfile.mkstemp()
-    epw_file_path = _os.path.join(output_dir, file_name_gz.replace(".gz", ".epw"))
-    with _request.urlopen(isd_lite_file_url) as response, open(epw_gz_file_handle, 'wb') as epw_gz_file:
-        epw_gz_file.write(response.read())
-    with _gzip.open(epw_gz_file_path, 'rb') as epw_gz_file, open(epw_file_path, 'w') as epw_file:
-        epw_file.write(epw_gz_file.read().decode('utf-8'))
+    # Download the ISD Lite file and save it
+    isd_lite_file_url = catalog_url + file_name
+    downloaded_file_path = _os.path.join(output_dir, file_name)
+    with _request.urlopen(isd_lite_file_url) as response:
+        with open(downloaded_file_path, 'wb') as downloaded_file:
+            downloaded_file.write(response.read())
 
-    return epw_file_path
+    return downloaded_file_path
