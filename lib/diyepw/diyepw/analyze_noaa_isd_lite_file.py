11f78a839f7680d6ae58b44b410080df04a6fb38
Benjamin Stürmer <bstuermer@eno.de>
2020-12-21 16:00:30 +0100

    Added parameter documentation comments

--- lib/diyepw/diyepw/analyze_noaa_isd_lite_file.py
+++ lib/diyepw/diyepw/analyze_noaa_isd_lite_file.py
@@ -63,7 +63,7 @@ def _get_max_missing_rows_from_hourly_dataframe(df:_pd.DataFrame, timestamp_col_
 
     :param df: The DataFrame from which to retrieve the maximum number of missing timestamps.
     :param timestamp_col_name: The name of the column containing timestamps in the DataFrame `df`
-    :return: The number of missing rows in the largest
+    :return: The size of the longest sequence of missing timestamps from the Dataframe `df`
     """
     # Create series of continuous timestamp values for that year
     all_timestamps = _pd.date_range(df[timestamp_col_name].iloc[0], periods=8760, freq='H')
