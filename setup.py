a13c7a6e862aa30a76af648c5aac16d98aee7975
Benjamin Stürmer <bstuermer@eno.de>
2021-01-07 17:33:19 +0100

    Un-commented README handling in setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,9 @@ License:  TODO: We need to determine what license to publish this project under
 
 import setuptools  # pragma: no cover
 
-# TODO: Comment back in when a final structure has been established for this project. Currently the README is in
-#   the root of the project, but the root is not the package container because we still have scripts and package in
-#   a common Git repo.
-# def readme():
-#     with open('README.md') as f:
-#         return f.read()
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 def get_requirements():
     with open('requirements.txt') as f:
@@ -22,7 +19,7 @@ setuptools.setup(  # pragma: no cover
       name='diyepw',
       version='1.0',
       description='A package offering support for generating EPW (EnergyPlus Weather) files',
-      # long_description=readme(),
+      long_description=readme(),
       url='https://github.com/IMMM-SFA/diyepw',
       author='Amanda Smith',
       author_email='amanda.d.smith@pnnl.gov',
