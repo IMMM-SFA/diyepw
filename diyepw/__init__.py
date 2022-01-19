f1904bcde05f29d63b91b6c2c73a4cc7fdf9103a
thurber <travis.thurber@pnnl.gov>
2022-01-19 09:42:29 -0800

    v1.2.2

--- diyepw/__init__.py
+++ diyepw/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 from .meteorology import Meteorology
 from .create_amy_epw_files_for_years_and_wmos import create_amy_epw_files_for_years_and_wmos
 from .analyze_noaa_isd_lite_files import analyze_noaa_isd_lite_files
